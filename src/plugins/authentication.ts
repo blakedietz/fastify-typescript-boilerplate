e14ca514724adeb280b6657ddc89f70243f28973
Blake Dietz <blakedietz@gmail.com>
2019-10-06 19:43:58 -0600

    feat(authentication): Add authentication plugin for checking jwt.

---
+++ src/plugins/authentication.ts
@@ -0,0 +1,42 @@
+import fp from 'fastify-plugin';
+import { Container } from 'typedi';
+import { AccountVerifyService } from '../context/account/account-verify.service';
+import {
+  InvalidTokenError,
+  NoAuthorizationHeaderError,
+} from '../context/account/errors';
+import { decode } from 'jsonwebtoken';
+
+export default fp(async function(fastify, opts) {
+  fastify.decorate('validateJwtAccountVerify', async function(
+    request,
+    reply,
+    done,
+  ) {
+    if (!request.headers.authorization) {
+      throw new NoAuthorizationHeaderError();
+    }
+    const token = request.headers.authorization.replace(/Bearer /, '');
+    const accountVerifyService = Container.get(AccountVerifyService);
+    const tokenIsValid = await accountVerifyService.isValidJwt({ jwt: token });
+
+    if (!tokenIsValid) {
+      throw new InvalidTokenError();
+    }
+
+    request.user = {
+      token: decode(token),
+    };
+    done();
+  });
+
+  // fastify.decorate('validateJwtPasswordReset', async function(
+  //   request,
+  //   reply,
+  // ) {});
+  // fastify.decorate('validateJwtSession', async function(
+  //   request,
+  //   reply,
+  //   done,
+  // ) {});
+});
